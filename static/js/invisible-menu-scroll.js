ba9b4d1284cec3056e6cc0dd8502228840421c07
Sanpi <sanpi@homecomputing.fr>
2017-05-04 12:47:35 +0200

    Remove jquery

--- static/js/invisible-menu-scroll.js
+++ static/js/invisible-menu-scroll.js
@@ -1,19 +1,19 @@
-function invisibleMenu() {
+var now = 0;
+var pixels =  300;
+var opacity = 1;
 
-    var now = 0;
-    var pixels =  300;
-    var opacity = 1;
+var move = function() {
+    now = window.scrollY;
+    if (now > pixels && opacity !== 0) {
+        opacity = 0;
+    }
+    else if(now < pixels) {
+        //Depends on the position not if scrolls down or up
+        opacity = (1 - now/pixels);
+    }
+    var el = document.querySelector("div.navigation");
+    el.style.opacity = opacity;
+};
 
-    var move = function() { 
-        now = $(window).scrollTop();
-        if (now > pixels && opacity !== 0)
-            opacity = 0;
-        else if(now < pixels){
-            //Depends on the position not if scrolls down or up
-            opacity = (1 - now/pixels);
-        }
-        $("div.navigation").css({"opacity": opacity});
-    };
-    $(window).scroll(move);
-    move();
-}
+window.addEventListener("scroll", move);
+move();
